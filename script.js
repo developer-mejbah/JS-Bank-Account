ac1c6d428a5564f67fbaee82f894f2fbd89eec57
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-28 04:12:03 +0600

    half Work Done

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").addEventListener("click", function() {
+    const title = document.getElementById("header");
+    title.style.display = "block"
+})
+
+document.getElementById("btn2").addEventListener("click", function() {
+    const title = document.getElementById("header");
+    title.style.display = "none"
+})
