1c3103461d5c5a8d551846133256bff34b07225a
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-30 01:46:56 +0600

    html css And Js Withdraw Section Done

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.getElementById("btn2").addEventListener("click", function() {
     const title = document.getElementById("header");
     title.style.display = "none"
 })
+
+const btn = document.getElementById("btn3");
+btn.addEventListener("click", function() {
+    const withdraw = document.getElementById("withdraw");
+    const withdrawValueStr = withdraw.value;
+    const withdrawValue = parseInt(withdrawValueStr);
+
+    const zero = document.getElementById("zero");
+    const zeroValueStr = zero.innerText;
+    const zeroValue = parseInt(zeroValueStr);
+
+    const totalWithdrawAmount = withdrawValue + zeroValue;
+    zero.innerText = totalWithdrawAmount;
+
+    const taka = document.getElementById("taka");
+    const takaStr = taka.innerText;
+    const takapar = parseInt(takaStr);
+    
+    const newTaka = takapar - totalWithdrawAmount;
+    taka.innerText = newTaka;
+
+    withdraw.value = "";
+})
