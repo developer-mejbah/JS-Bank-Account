e0f5be9f0f0aad12ac238c9bd151d724cf11c82a
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-30 02:08:18 +0600

    Bank DashBoard Done

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ btn.addEventListener("click", function() {
 
     withdraw.value = "";
 })
+
+const btn5 = document.getElementById("btn4");
+btn5.addEventListener("click", function() {
+    const deposit = document.getElementById("deposit");
+    const depositValueStr = deposit.value;
+    const depositValue = parseInt(depositValueStr);
+
+    const zero2 = document.getElementById("zero2");
+    const zero2ValueStr = zero2.innerText;
+    const zero2Value = parseInt(zero2ValueStr);
+
+    const totalDepositAmount =  depositValue + zero2Value;
+    zero2.innerText = totalDepositAmount;
+
+    const taka = document.getElementById("taka");
+    const takaStr = taka.innerText;
+    const takapar = parseInt(takaStr);
+
+    const newTaka = takapar + totalDepositAmount;
+    taka.innerText = newTaka;
+
+    deposit.value = "";
+})
